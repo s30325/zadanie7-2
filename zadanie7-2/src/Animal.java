b1e58992bbf64bd30cf02d420061719e4f875e0d
Wojciech Chelkowski <s30325@pjwstk.edu.pl>
2024-04-30 15:26:39 +0200

    zadanie7-2

---
+++ zadanie7-2/src/Animal.java
@@ -0,0 +1,14 @@
+abstract class Animal {
+    public String name;
+    public String type;
+    public Animal(String name) {
+        this.name = name;
+    }
+    String getType(){
+        return type;
+    }
+    String getName(){
+        return name;
+    }
+}
+
